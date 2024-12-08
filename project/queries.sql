5ed7119e77a4b1329b677f5f9db23cf82cf6863e
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2024-12-08 19:40:21 +0300

    Update queries.sql

--- project/queries.sql
+++ project/queries.sql
@@ -3,3 +3,5 @@ SELECT user.user_name, posts.post_id, posts.published_at FROM user
 INNER JOIN posts ON posts.user_id = user.user_id
 WHERE user.user_id = 4 AND posts.post_type = 3
 ORDER BY posts.published_at DESC;
+
+--2nd Query
