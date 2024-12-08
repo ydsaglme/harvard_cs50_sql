ff90ba915c0cd162eef47c45c0e68c740898a8be
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2024-12-08 19:39:38 +0300

    Update queries.sql

--- project/queries.sql
+++ project/queries.sql
@@ -1,1 +1,5 @@
--- In this SQL file, write (and comment!) the typical SQL queries users will run on your database
+--1st Query
+SELECT user.user_name, posts.post_id, posts.published_at FROM user
+INNER JOIN posts ON posts.user_id = user.user_id
+WHERE user.user_id = 4 AND posts.post_type = 3
+ORDER BY posts.published_at DESC;
