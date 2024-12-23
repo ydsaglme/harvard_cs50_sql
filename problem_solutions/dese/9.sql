ff283aa2693d114c61f9513fa3f509c05929ef4a
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2024-12-23 18:04:27 +0300

    Update 9.sql

--- problem_solutions/dese/9.sql
+++ problem_solutions/dese/9.sql
@@ -1,5 +1,6 @@
 SELECT "name" FROM "districts"
 WHERE "id" = (
     SELECT "district_id" FROM "expenditures"
-    GROUP BY "district_id" ORDER BY "pupils" LIMIT 1
+    GROUP BY "district_id"
+    ORDER BY "pupils" LIMIT 1
 );
