22306b516736bc95bfd69e4a9188d03ee940778a
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2024-12-23 18:13:59 +0300

    Update 10.sql

--- problem_solutions/dese/10.sql
+++ problem_solutions/dese/10.sql
@@ -1,4 +1,5 @@
 SELECT "districts"."name", "expenditures"."per_pupil_expenditure" FROM "districts"
 JOIN "expenditures" ON "districts"."id" = "expenditures"."district_id"
 WHERE "districts"."type" = 'Public School District'
-GROUP BY "name" ORDER BY "per_pupil_expenditure" DESC LIMIT 10;
+GROUP BY "name"
+ORDER BY "per_pupil_expenditure" DESC LIMIT 10;
