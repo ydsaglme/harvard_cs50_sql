ccbe43f9bac657ba308461179a4c8065d7a97af7
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-01 17:37:00 +0300

    Update 1.sql

--- problem_solutions/moneyball/1.sql
+++ problem_solutions/moneyball/1.sql
@@ -1,2 +1,3 @@
-SELECT "year", ROUND(AVG("salary"), 2) AS "average salary" FROM "salaries"
-GROUP BY "year" ORDER BY "year" DESC;
+SELECT "year", ROUND(AVG("salary"), 2) AS "average_salary" FROM "salaries"
+GROUP BY "year"
+ORDER BY "year" DESC;
