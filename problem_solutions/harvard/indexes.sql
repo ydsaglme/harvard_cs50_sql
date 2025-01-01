4ab0b368f2821a37b00e20029e3c8776f1d21ec4
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-01 13:01:43 +0300

    Update indexes.sql

--- problem_solutions/harvard/indexes.sql
+++ problem_solutions/harvard/indexes.sql
@@ -1,4 +1,7 @@
-CREATE INDEX "enrollments_student_id" ON "enrollments" ("student_id");
-CREATE INDEX "enrollments_course_id" ON "enrollments" ("course_id");
-CREATE INDEX "courses_semester" ON "courses" ("semester");
-CREATE INDEX "satisfies_course_id" ON "satisfies" ("course_id");
+CREATE INDEX "enrollments_student_id" ON "enrollments"("student_id");
+
+CREATE INDEX "enrollments_course_id" ON "enrollments"("course_id");
+
+CREATE INDEX "courses_semester" ON "courses"("semester");
+
+CREATE INDEX "satisfies_course_id" ON "satisfies"("course_id");
