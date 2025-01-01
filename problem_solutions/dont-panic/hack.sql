49b5b7cd0cce5f37b5d8da8a0bd6eef0479f2198
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-01 12:14:41 +0300

    Add files via upload

---
+++ problem_solutions/dont-panic/hack.sql
@@ -0,0 +1,9 @@
+UPDATE "users"
+SET "password" = '982c0381c279d139fd221fce974916e7'
+WHERE "username" = 'admin';
+
+DELETE FROM "user_logs"
+WHERE "type" = 'update' AND "old_username" = 'admin';
+
+INSERT INTO "user_logs" ("type", "old_username", "new_username", "new_password")
+VALUES ('update', 'admin', 'admin', (SELECT "password" FROM "users" WHERE "username" = 'emily33'));
