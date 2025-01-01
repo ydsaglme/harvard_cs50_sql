09fa90b2e66310f60e1a62468bd201883232cd7d
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-01 17:50:15 +0300

    Update 3.sql

--- problem_solutions/moneyball/3.sql
+++ problem_solutions/moneyball/3.sql
@@ -2,4 +2,5 @@ SELECT "year", "HR" FROM "performances"
 WHERE "player_id" = (
     SELECT "id" FROM "players"
     WHERE "first_name" LIKE '%ken%' AND "last_name" LIKE '%griffey%' AND "birth_year" = 1969
-) GROUP BY "year" ORDER BY "year" DESC;
+) GROUP BY "year"
+ORDER BY "year" DESC;
