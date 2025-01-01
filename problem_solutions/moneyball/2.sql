1ae2320e6732727f1edb99619b60ce44a6317cf5
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-01 17:43:02 +0300

    Update 2.sql

--- problem_solutions/moneyball/2.sql
+++ problem_solutions/moneyball/2.sql
@@ -2,4 +2,5 @@ SELECT "year", "salary" FROM "salaries"
 WHERE "player_id" = (
     SELECT "id" FROM "players"
     WHERE "first_name" LIKE '%cal%' AND "last_name" LIKE '%ripken%'
-) GROUP BY "year" ORDER BY "year" DESC;
+) GROUP BY "year"
+ORDER BY "year" DESC;
