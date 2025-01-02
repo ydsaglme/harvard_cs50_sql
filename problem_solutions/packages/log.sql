3e41ecd60d444a9a0ad159722d0326f94258a41e
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-02 19:10:43 +0300

    Add files via upload

---
+++ problem_solutions/packages/log.sql
@@ -0,0 +1,38 @@
+-- *** The Lost Letter ***
+SELECT "type", "address" FROM "addresses"
+WHERE "id" = (
+    SELECT "to_address_id" FROM "packages"
+    WHERE "contents" LIKE '%congratulatory%'
+);
+
+-- *** The Devious Delivery ***
+SELECT "type" FROM "addresses"
+WHERE "id" = (
+    SELECT "address_id" FROM "scans"
+    WHERE "package_id" = (
+        SELECT "id" FROM "packages"
+        WHERE "from_address_id" IS NULL
+    ) AND "action" = 'Drop'
+);
+
+SELECT "contents" FROM "packages"
+WHERE "from_address_id" IS NULL;
+
+-- *** The Forgotten Gift ***
+SELECT "contents" FROM "packages"
+WHERE "from_address_id" = (
+    SELECT "id" FROM "addresses"
+    WHERE "address" = '109 Tileston Street'
+);
+
+SELECT "name" FROM "drivers"
+WHERE "id" = (
+    SELECT "driver_id" FROM "scans"
+    WHERE "package_id" = (
+        SELECT "id" FROM "packages"
+        WHERE "from_address_id" = (
+            SELECT "id" FROM "addresses"
+            WHERE "address" = '109 Tileston Street'
+        )
+    ) ORDER BY "timestamp" DESC LIMIT 1
+);
