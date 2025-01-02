2cae22be0b7778fa2c1166515384b5857b507ef5
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-02 18:58:33 +0300

    Update 11.sql

--- problem_solutions/moneyball/11.sql
+++ problem_solutions/moneyball/11.sql
@@ -1,5 +1,5 @@
-SELECT "players"."first_name", "players"."last_name", ("salaries"."salary"/"performances"."H") AS "dollars per hit" FROM "players"
+SELECT "players"."first_name", "players"."last_name", ("salaries"."salary"/"performances"."H") AS "dollars_per_hit" FROM "players"
 JOIN "salaries" ON "players"."id" = "salaries"."player_id"
 JOIN "performances" ON "players"."id" = "performances"."player_id" AND "salaries"."year" = "performances"."year"
 WHERE "salaries"."year" = 2001 AND "performances"."H" != 0
-ORDER BY "dollars per hit", "players"."first_name", "players"."last_name", "salary" LIMIT 10;
+ORDER BY "dollars_per_hit", "players"."first_name", "players"."last_name", "salary" LIMIT 10;
