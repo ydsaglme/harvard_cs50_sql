50e9b66686b95c0a87aea0755996dde751c77f81
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-05 21:43:57 +0300

    Update 10.sql

--- problem_solutions/views/10.sql
+++ problem_solutions/views/10.sql
@@ -1,1 +1,4 @@
-SELECT "artist" AS "Artist", ROUND(AVG("brightness"), 2) AS "Average Brightness Choice by the Artist" FROM "views" WHERE "english_title" LIKE '%fuji%' GROUP BY "artist" ORDER BY "brightness" DESC;
+SELECT "artist", ROUND(AVG("brightness"), 2) FROM "views"
+WHERE "english_title" LIKE '%fuji%'
+GROUP BY "artist"
+ORDER BY "brightness" DESC;
