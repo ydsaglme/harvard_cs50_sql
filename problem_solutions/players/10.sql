9110c56c8885c18251e2f2e2943713d239df5dfc
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2025-01-05 20:17:55 +0300

    Update 10.sql

--- problem_solutions/players/10.sql
+++ problem_solutions/players/10.sql
@@ -1,1 +1,4 @@
-SELECT "birth_state" AS "State", ROUND(AVG("height"), 2) AS "Average Height", ROUND(AVG("weight"), 2) AS "Average Weight" FROM "players" WHERE "birth_country" = 'USA' GROUP BY "birth_state" ORDER BY "Average Height" DESC, "Average Weight" DESC;
+SELECT "birth_state", ROUND(AVG("height"), 2) AS "average_height", ROUND(AVG("weight"), 2) AS "average_weight" FROM "players"
+WHERE "birth_country" = 'USA'
+GROUP BY "birth_state"
+ORDER BY "average_height" DESC, "average_weight" DESC;
